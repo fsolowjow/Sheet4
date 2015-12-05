5780558679cc8affa3642038ee14e9da1b579355
fsolowjow <friedrich.solowjow2@gmail.com>
2015-12-05 15:26:40 +0100

    Bugs fixed

--- Godunov.py
+++ Godunov.py
@@ -11,7 +11,7 @@ from matplotlib import animation
 def set_up():
     global fig, ax, line, time_text
     fig = plt.figure()
-    ax = plt.axes(ylim=(0, 1), xlim=(-2, 2))
+    ax = plt.axes(ylim=(-1, max), xlim=(-2, 2))
     ax.set_ylabel('$q(x,t)$')
     ax.set_xlabel('$x$')
     ax.set_title('Godunovs method for linear systems')
@@ -57,6 +57,17 @@ def initial_values_1dim(x):
 	if ( x < 0 ): return 0
 	else:	return 1
 
+def wave_sum(wavespeed, q , x , i):
+	temp = 0
+	for j in range( np.size(wavespeed)):
+		if ( wavespeed[j] > 0 ):	
+			if( i == 0 ):				temp += (wavespeed[j] * (q[0,j] - q[np.size(x) - 1,j]))					
+			else:						temp += (wavespeed[j] * (q[i,j] - q[i-1,j]))
+		else:
+			if( i == np.size(x) - 1):	temp += (wavespeed[j] * (q[0,j] - q[i,j]))
+			else:						temp += (wavespeed[j] * (q[i+1,j] - q[i,j]))
+	return temp
+
 def Godunov_linear_solv(A,q_l,q_r):
 
 	dim = np.size(q_l)
@@ -64,6 +75,8 @@ def Godunov_linear_solv(A,q_l,q_r):
 	#Case of a system: 
 	if(dim > 1):
 		eigenvalue , eigenvector = LA.eig(A)
+		wavespeed = eigenvalue * t_step / x_step
+		print(eigenvalue)
 		U = np.empty((x.size,t.size))
 		#Sets the Q_i up for the first time step with the initial data. 
 		#Q[j,i] is a matrix and contains the values for component i at x[j] at each time step
@@ -72,16 +85,18 @@ def Godunov_linear_solv(A,q_l,q_r):
 		#iterating over time
 		for j in range(np.size(t)) :
 			#Values for the animation are saved in U
-			U[:,j] = q[:,1]									#Change here to animate other components
+			U[:,j] = q[:,2]									#Change here to animate other components
 			#The values for the next time step are saved in qtemp and afterwards q -> qtemp
 			qtemp = q
 			for n in range( dim ):
 				for i in range(np.size(x)):
-					if( i == 0 ): qtemp[0,n] = qtemp[0,n] - (( abs(eigenvalue[n]) * t_step / x_step) * (qtemp[0,n] - qtemp[np.size(x) - 1 ,n]))
-					qtemp[i,n] = qtemp[i,n] - ((abs(eigenvalue[n]) * t_step / x_step) * (qtemp[i,n] - qtemp[i-1,n]))
-					if( i == np.size(x) - 1):	qtemp[i,n] = qtemp[i,n] - ((abs(eigenvalue[n]) * t_step / x_step) * (qtemp[i,n] - qtemp[0,n]))
-				q = qtemp
-	#1dim case with the wavespeed A, that gets passed instead of a matrix:			
+					qtemp[i,n] = q[i,n] - wave_sum(wavespeed, q , x , i)
+			q = qtemp	
+			
+			
+			
+	#1dim case with the wavespeed A, that gets passed instead of a matrix:	
+	
 	else:
 		U = np.empty((x.size,t.size))
 		q = np.zeros( np.size(x) )
@@ -91,20 +106,24 @@ def Godunov_linear_solv(A,q_l,q_r):
 		for j in range(np.size(t)) :
 			U[:,j] = q
 			qtemp = q
-			for i in range(np.size(q)):
-				if( i == 0 ): qtemp[0] = qtemp[0] - ((abs(A) * t_step / x_step) * (qtemp[0] - qtemp[np.size(q) - 1]))
-				qtemp[i] = qtemp[i] - ((abs(A) * t_step / x_step) * (qtemp[i] - qtemp[i-1]))
-				if( i == np.size(q) - 1):	qtemp[i] = qtemp[i] - ((abs(A) * t_step / x_step) * (qtemp[i] - qtemp[0]))
+			if ( A > 0):
+				for i in range(np.size(q)):
+					if( i == 0 ):				qtemp[i] = q[i] - ((A * t_step / x_step) * (q[i] - q[np.size(q) - 1]))
+					else:						qtemp[i] = q[i] - ((A * t_step / x_step) * (q[i] - q[i-1]))
+			else:
+				for i in range(np.size(q)):
+					if( i == np.size(q) - 1):	qtemp[i] = q[i] - ((A * t_step / x_step) * (q[0] - q[i]))
+					else:						qtemp[i] = q[i] - ((A * t_step / x_step) * (q[i+1] - q[i]))
 			q = qtemp
 	
 	return U
 
-#t_step = 0.01
-t_step = input("Enter time stepsize (e.g. 0.01):")
+t_step = 0.001
+#t_step = input("Enter time stepsize (e.g. 0.01):")
 t = np.arange ( 0 , 4 , t_step)
 
-#x_step = 0.05
-x_step = input("Enter space stepsize (e.g. 0.05):")
+x_step = 0.05
+#x_step = input("Enter space stepsize (e.g. 0.05):")
 x = np.arange(-2, 2 , x_step)
 
 A3 = np.array ( [[2 , 1 , -1] , [1, 1 , 1] , [-1, 1 , 2]])
@@ -121,6 +140,7 @@ q_l1 = 0
 q_r1 = 1
 
 U=Godunov_linear_solv( A3 , q_l3 , q_r3)
+max=np.max(U)
 run()
 
 # The programm can crash for max eigenvalue * c > 1, because the CFL condition is not satisfied => unstable
